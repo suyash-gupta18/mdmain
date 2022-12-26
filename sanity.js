2e1c5815b29f497e91eba959e2fe7301f294dbf6
Parth-kumar-1802 <parthkumar1802@gmail.com>
2022-12-26 13:22:05 +0530

    images now showing

--- sanity.js
+++ sanity.js
@@ -3,9 +3,9 @@ import ImageUrlBuilder from "@sanity/image-url";
 
 const client = SanityClient({
     projectId: "2jvr53rn",
-  dataset:"production",
-  useCdn:true,
-  apiVersion:"2022-12-24",
+    dataset:"production",
+    useCdn:true,
+    apiVersion:"2022-12-24",
 });
 
 const builder = ImageUrlBuilder(client);
