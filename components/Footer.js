4414dcf40400bda4215b238b51d64ec2caae1daa
Suyash Gupta <100958229+suyash-gupta18@users.noreply.github.com>
2022-12-28 17:07:07 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-       <div className='Logo' style={{ height:40 , width:40  }}>
+       <div className='Logo' style={{ height:40 , width:1440 , backgroundColor: 'white' }}>
        <a target="_blank" href="https://www.facebook.com/MDiitism/"><img className='fb' style={{ height:40 , width:40 , backgroundColor:'white' }} src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/640px-Facebook_f_logo_%282021%29.svg.png" ></img></a>
        </div>   
     </div>
